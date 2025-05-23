5a8c0ea338d007e23907a9bac3068251056dae3b
HDUC122002 <huynhhducc2002@gmail.com>
2025-05-23 16:26:09 +0700

    add securit config

---
+++ job-profile-service/src/main/java/com/dan/job_profile_service/security/Endpoints.java
@@ -0,0 +1,25 @@
+package com.dan.job_profile_service.security;
+
+public class Endpoints {
+    public static final String[] PRIVATE_POST_ENDPOINTS = {
+    };
+
+    public static final String[] PUBLIC_POST_ENDPOINTS = {
+    };
+
+    public static final String[] PRIVATE_DELETE_ENDPOINTS = {
+    };
+
+    public static final String[] ADMIN_POST_ENDPOINTS = {
+    };
+
+    public static final String[] PRIVATE_GET_ENDPOINTS = {
+
+    };
+
+    public static final String[] PUBLIC_GET_ENDPOINTS = {
+    };
+
+    public static final String[] ADMIN_DELETE_ENDPOINTS = {
+    };
+}
---
+++ job-profile-service/src/main/java/com/dan/job_profile_service/security/jwt/JwtService.java
@@ -0,0 +1,69 @@
+package com.dan.job_profile_service.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpirationInMillis;
+
+    @Value("${jwt.refreshExpiration}")
+    private long refreshExpirationInMillis;
+
+    public String generateToken(String username, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("roles", roles);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMillis))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public Claims getClaimsFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaimsFromToken(token).getSubject();
+    }
+
+    public List<String> getRolesFromToken(String token) {
+        return getClaimsFromToken(token).get("roles", List.class);
+    }
+
+    public String getUsernameFromRequest(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            return getUsernameFromToken(token);
+        } else {
+            return null;
+        }
+    }
+}
