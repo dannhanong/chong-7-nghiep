434a01aaa5fcc5951579ad0814aa5d8a0be33840
dao24092004 <134945882+dao24092004@users.noreply.github.com>
2025-05-29 16:55:28 +0700

    get job by user

--- job-service/src/main/java/com/dan/job_service/services/JobApplicationService.java
+++ job-service/src/main/java/com/dan/job_service/services/JobApplicationService.java
@@ -1,6 +1,7 @@
 package com.dan.job_service.services;
 
 import com.dan.job_service.dtos.requets.JobApplicationRequest;
+import com.dan.job_service.dtos.responses.ApplicantResponse;
 import com.dan.job_service.dtos.responses.ResponseMessage;
 import com.dan.job_service.models.JobApplication;
 import org.springframework.data.domain.Page;
@@ -8,6 +9,6 @@ import org.springframework.data.domain.Pageable;
 
 public interface JobApplicationService {
     ResponseMessage applyJob(JobApplicationRequest request, String jobId, String username);
-    Page<JobApplication> getJobApplicationByUserId(String userId, String username, Pageable pageable);
+    Page<JobApplication> getJobApplicationByUserId( String username, Pageable pageable);
     Page<JobApplication> getJobApplicationByJobId(String jobId, String username, Pageable pageable);
 }
