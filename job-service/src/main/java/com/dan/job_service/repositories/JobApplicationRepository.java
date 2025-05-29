dcf2c903768090bbfa21dee90dd89e5117b8a4b2
HDUC122002 <huynhhducc2002@gmail.com>
2025-05-29 14:57:33 +0700

    add job apply

--- job-service/src/main/java/com/dan/job_service/repositories/JobApplicationRepository.java
+++ job-service/src/main/java/com/dan/job_service/repositories/JobApplicationRepository.java
@@ -1,8 +1,9 @@
 package com.dan.job_service.repositories;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,7 @@ import com.dan.job_service.models.JobApplication;
 
 @Repository
 public interface JobApplicationRepository extends MongoRepository<JobApplication, String> {
-    List<JobApplication> findByUserId(String userId);
-    List<JobApplication> findByJobId(String jobId);
+    Page<JobApplication> findByUserId(String userId, Pageable pageable);
+    Page<JobApplication> findByJobId(String jobId, Pageable pageable);
     Optional<JobApplication> findByUserIdAndJobId(String userId, String jobId);
 }
